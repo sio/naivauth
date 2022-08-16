198fd171ee76809d74a3665e90d110eaf775d42b
Vitaly Potyarkin <sio.wtf@gmail.com>
2022-08-16 13:53:59 +0300

    Add stored procedures to manage user sessions

--- schema.sql
+++ schema.sql
@@ -43,6 +43,72 @@ CREATE UNIQUE INDEX IF NOT EXISTS constraint_token_unique_among_active ON sessio
 );
 
 
+CREATE OR REPLACE VIEW active_session AS
+    SELECT * FROM session
+    WHERE
+        active AND
+        expires_at > now() AND
+        trash IS NULL;
+CREATE OR REPLACE VIEW activating_session AS
+    SELECT * FROM session
+    WHERE
+        NOT active AND
+        expires_at > now() AND
+        trash IS NULL;
+CREATE OR REPLACE VIEW valid_session AS
+    SELECT * FROM session
+    WHERE
+        expires_at > now() AND
+        trash IS NULL;
+
+
 CREATE INDEX IF NOT EXISTS cookie_lookup ON session (cookie, fingerprint);
 CREATE INDEX IF NOT EXISTS fingerprint_lookup ON session USING HASH (fingerprint);
 CREATE INDEX IF NOT EXISTS token_lookup ON session USING HASH (token);
+
+
+CREATE OR REPLACE FUNCTION get_username(fingerprint bytea, cookie uuid) RETURNS text AS $$
+    SELECT username FROM active_session
+    WHERE fingerprint = get_username.fingerprint AND cookie = get_username.cookie
+$$ LANGUAGE sql STRICT;
+
+
+CREATE OR REPLACE FUNCTION get_token(fingerprint bytea, OUT token int, OUT cookie uuid) AS $$
+BEGIN
+    /* Check if there already exists a token waiting to be activated */
+    SELECT session.token, session.cookie FROM valid_session AS session
+    WHERE session.fingerprint = get_token.fingerprint
+    ORDER BY created_at DESC LIMIT 1
+    INTO get_token.token, get_token.cookie;
+
+    /* Create new token */
+    IF NOT FOUND THEN
+        INSERT INTO session (fingerprint) values (get_token.fingerprint);
+        SELECT session.token, session.cookie FROM valid_session AS session
+        WHERE session.fingerprint = get_token.fingerprint
+        INTO get_token.token, get_token.cookie;
+    END IF;
+END;
+$$ LANGUAGE plpgsql STRICT;
+
+
+CREATE OR REPLACE PROCEDURE activate(token int, username text) AS $$
+    UPDATE session
+    SET
+        active = true,
+        username = activate.username
+    WHERE
+        session.token = activate.token AND
+        session.expires_at > now() AND
+        trash is NULL;
+$$ LANGUAGE sql;
+
+
+CREATE OR REPLACE PROCEDURE deactivate(cookie uuid) AS $$
+    UPDATE session
+    SET
+        active = false,
+        trash = cookie
+    WHERE
+        session.cookie = deactivate.cookie;
+$$ LANGUAGE sql;
