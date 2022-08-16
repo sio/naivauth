ef2c1633f8ed8be4761f51d429c9d09cf8a0d8dc
Vitaly Potyarkin <sio.wtf@gmail.com>
2022-08-16 11:23:25 +0300

    Lookups will happen only on cookie+fingerprint pairs

--- schema.sql
+++ schema.sql
@@ -43,6 +43,6 @@ CREATE UNIQUE INDEX IF NOT EXISTS constraint_token_unique_among_active ON sessio
 );
 
 
-CREATE INDEX IF NOT EXISTS cookie_lookup ON session USING HASH (cookie);
+CREATE INDEX IF NOT EXISTS cookie_lookup ON session (cookie, fingerprint);
 CREATE INDEX IF NOT EXISTS fingerprint_lookup ON session USING HASH (fingerprint);
 CREATE INDEX IF NOT EXISTS token_lookup ON session USING HASH (token);
