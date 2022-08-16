edef8d26faf3298f3d88c60417125b9fb6558631
Vitaly Potyarkin <sio.wtf@gmail.com>
2022-08-16 13:22:24 +0000

    Check that main data table gets created

--- tests/test_creating.py
+++ tests/test_creating.py
@@ -2,4 +2,8 @@ import pytest
 
 def test_applying_schema(database):
     '''Check if schema applies cleanly'''
-    pass  # schema is applied in fixture code
+    with database:
+        with database.cursor() as cursor:
+            cursor.execute('''SELECT * FROM pg_tables WHERE tablename = 'session' ''')
+            result = cursor.fetchone()
+            assert result
